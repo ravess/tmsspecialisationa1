de3ccacc6bd674e242aa95b20c837cdf747f49cd
Jolene <mjolenetay@gmail.com>
2023-09-14 12:11:48 +0800

    separate adminctrl to adminService for getAlluser, getAllGrp, newGrp

--- src/a1/src/main/java/com/tms/a1/service/AdminService.java
+++ src/a1/src/main/java/com/tms/a1/service/AdminService.java
@@ -1,5 +1,41 @@
 package com.tms.a1.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.tms.a1.entity.Group;
+import com.tms.a1.entity.User;
+import com.tms.a1.repository.GroupRepo;
+import com.tms.a1.repository.UserRepo;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
 public class AdminService {
     
+    private UserRepo userRepo;
+    private GroupRepo groupRepo;
+    private BCryptPasswordEncoder passwordEncoder;
+    public Map<String, Object> response = new HashMap<>();
+
+    public List<User> getAllUsers() {
+        return (List<User>)userRepo.findAll();
+    }
+
+    public List<Group> getAllGroups() {
+        return (List<Group>)groupRepo.findAll();
+    }
+
+    public String newGroup(Group group){
+        if (groupRepo.existsByGroupName(group.getGroupName())) {
+            return "Duplicate";
+        }
+        groupRepo.save(group);
+        return "Success";
+    }
 }
--- src/a1/src/main/java/com/tms/a1/service/AuthService.java
+++ src/a1/src/main/java/com/tms/a1/service/AuthService.java
@@ -17,24 +17,24 @@ import com.tms.a1.repository.UserRepo;
 @Service
 public class AuthService {
     @Autowired
-  private UserRepo userRepo;
-  @Autowired
-  private BCryptPasswordEncoder passwordEncoder;
+    private UserRepo userRepo;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-  public Map<String,Object> checkgroup(Map<String,String> requestBody){
-    String username = requestBody.get("username"); //Should be from JWToken instead of req body
-    String group = requestBody.get("group");
-    String result = userRepo.checkgroup(username,group);
-    if (result != null){
-      String resMsg = "True";
-      Map<String, Object> response = new HashMap<>();
-    response.put("ingroup", resMsg);
-      return response;
-    }else{
-      String resMsg = "False";
-      Map<String, Object> response = new HashMap<>();
-      response.put("ingroup", resMsg);
-      return response;
+    public Map<String,Object> checkgroup(Map<String,String> requestBody){
+        String username = requestBody.get("username"); //Should be from JWToken instead of req body
+        String group = requestBody.get("group");
+        String result = userRepo.checkgroup(username,group);
+        if (result != null){
+            String resMsg = "True";
+            Map<String, Object> response = new HashMap<>();
+            response.put("ingroup", resMsg);
+            return response;
+        }else{
+            String resMsg = "False";
+            Map<String, Object> response = new HashMap<>();
+            response.put("ingroup", resMsg);
+            return response;
+        }
     }
-  }
 }
