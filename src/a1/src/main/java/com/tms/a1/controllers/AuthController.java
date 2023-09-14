c7057fced85382f9c54b7cc3e57ee1033071347e
Jolene <mjolenetay@gmail.com>
2023-09-14 17:51:31 +0800

    resolve conflict, added temp validateCookie fn

--- src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
+++ src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
@@ -1,16 +1,17 @@
 package com.tms.a1.controllers;
 
+import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tms.a1.config.security.SecurityConstants;
-
 import com.tms.a1.service.AuthService;
 
 import jakarta.servlet.http.Cookie;
@@ -34,12 +35,12 @@ public class AuthController {
     return new ResponseEntity<>(message, HttpStatus.OK);
   }
 
-<<<<<<< HEAD
   @PostMapping("/checkGroup")
   public ResponseEntity<Map<String,Object>> CheckGroup (@RequestBody Map<String, String> requestBody ) {
     return new ResponseEntity<>(authService.checkgroup(requestBody), HttpStatus.OK);
   }
 
+  //temp function for frontend to be able to work
   @GetMapping("/validateCookie")
   public ResponseEntity<?> checkCookie(){
     Boolean resMsg = true;
@@ -48,10 +49,4 @@ public class AuthController {
     // The user is not in the group, return unauthorized
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
-=======
-  @PostMapping("/checkgroup")
-  public ResponseEntity<Map<String, Object>> CheckGroup(@RequestBody Map<String, String> requestBody) {
-    return new ResponseEntity<>(authService.checkgroup(requestBody), HttpStatus.OK);
-  }
->>>>>>> d1758617c3ca36db5d4141fdc739b08e7286f7d5
 }
