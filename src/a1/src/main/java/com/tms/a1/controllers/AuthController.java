39be2a1b4ff62312e773a75c92f9e96df0ecb3e0
Jolene <mjolenetay@gmail.com>
2023-09-18 10:18:45 +0800

    comment validateCookie

--- src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
+++ src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
@@ -5,13 +5,14 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tms.a1.service.AuthService;
-
 
 import lombok.AllArgsConstructor;
 
@@ -30,12 +31,18 @@ public class AuthController {
   }
 
   //temp function for frontend to be able to work
-  @GetMapping("/validateCookie")
-  public ResponseEntity<?> checkCookie(){
-    Boolean resMsg = true;
-    Map<String, Object> response = new HashMap<>();
-    response.put("hasCookie", resMsg);
-    // The user is not in the group, return unauthorized
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
+  // @GetMapping("/validateCookie")
+  // public ResponseEntity<?> checkCookie(){
+  //   Map<String, Object> response = new HashMap<>();
+  //   Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+  //   if(authentication != null && authentication.isAuthenticated()){
+  //     response.put("hasCookie", true);
+  //     return ResponseEntity.status(HttpStatus.OK).body(response);
+  //   }
+
+  //   response.put("hasCookie", false);
+  //   // The user is not in the group, return unauthorized
+  //   return ResponseEntity.status(HttpStatus.OK).body(response);
+  // }
 }
