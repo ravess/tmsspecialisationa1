f9991032828ebc329e359a373b6a6c224eb6263e
Jolene <mjolenetay@gmail.com>
2023-09-18 14:09:08 +0800

    cleanup authCtrl

--- src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
+++ src/a1/src/main/java/com/tms/a1/controllers/AuthController.java
@@ -1,13 +1,9 @@
 package com.tms.a1.controllers;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tms.a1.service.AuthService;
 
 import lombok.AllArgsConstructor;
-
 @AllArgsConstructor
 @RestController
 public class AuthController {
 
   private AuthService authService;
-
-
-
 
   @PostMapping("/checkGroup")
   public ResponseEntity<Map<String,Object>> CheckGroup (@RequestBody Map<String, String> requestBody ) {
