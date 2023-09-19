4d1a56fe7dcf8adb728ca05ad02bfafe32f392e1
Roy <weichai.royston.yeo@stengg.com>
2023-09-19 13:29:47 +0800

    Removed repository folder as using Hibernate

--- src/a1/src/main/java/com/tms/a1/repository/GroupRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tms.a1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tms.a1.entity.Group;
-
-public interface GroupRepo extends JpaRepository<Group, String>{
-    boolean existsByGroupName(String groupName);
-}
