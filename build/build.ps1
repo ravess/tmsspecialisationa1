cf4292ff5a01cb0823850c2084690e0aa956b9f7
Roy <weichai.royston.yeo@stengg.com>
2023-10-09 13:12:49 +0800

    Delete config samples and amended gitlab-ci.yml

--- build/build.ps1
+++ build/build.ps1
@@ -1,24 +1,24 @@
 # Stop the script if any command fails
 $ErrorActionPreference = "Stop"
 
-Write-Host "build.ps1 running..."
-Write-Host $PWD
-Write-Host (Get-ChildItem)
+echo "build.ps1 running..."
+echo $PWD
+echo (Get-ChildItem)
 
-# $mvnwPath = "$BUILD_SVR_PATH\$env:CI_COMMIT_REF_NAME\mvnw"
+$mvnwPath = "$BUILD_SVR_PATH\$CI_COMMIT_REF_NAME\mvnw"
 
-# # Run the Maven Wrapper command
-# cmd /c $mvnwPath clean package -DskipTests -Pexclude-properties
+# Run the Maven Wrapper command
+cmd /c $mvnwPath clean package
 
-# docker build -t "$env:CI_COMMIT_REF_NAME" .
-# if (Test-Path "bin\$env:CI_COMMIT_REF_NAME.tar") {
-#     Remove-Item -Path "bin\$env:CI_COMMIT_REF_NAME.tar" -Recurse -Force;
-# }
-# New-Item -Path "$BUILD_SVR_PATH\$env:CI_COMMIT_REF_NAME\bin" -ItemType Directory -Force;
+docker build -t "$CI_COMMIT_REF_NAME" .
+if (Test-Path "bin\$CI_COMMIT_REF_NAME.tar") {
+    Remove-Item -Path "bin\$CI_COMMIT_REF_NAME.tar" -Recurse -Force;
+}
+New-Item -Path "$BUILD_SVR_PATH\$CI_COMMIT_REF_NAME\bin" -ItemType Directory -Force;
 
-# docker save -o ".\bin\$env:CI_COMMIT_REF_NAME.tar" "$env:CI_COMMIT_REF_NAME"
+docker save -o ".\bin\$CI_COMMIT_REF_NAME.tar" "$CI_COMMIT_REF_NAME"
 
-# # Copy the application.properties file into the target directory
-# Copy-Item -Path "$BUILD_SVR_PATH\$env:CI_COMMIT_REF_NAME\src\main\resources\application.properties" -Destination "$BUILD_SVR_PATH\$env:CI_COMMIT_REF_NAME\config\application.properties"
+# Copy the application.properties file into the target directory
+Copy-Item -Path "$BUILD_SVR_PATH\$CI_COMMIT_REF_NAME\src\main\resources\application.properties" -Destination "$BUILD_SVR_PATH\$CI_COMMIT_REF_NAME\config\application.properties"
 
 Write-Host "Build and copy operations completed!"
