c61c825f52fa1170f0da129d2b4fbb6510b7a277
Roy <weichai.royston.yeo@stengg.com>
2023-10-09 17:11:31 +0800

    Added test case

---
+++ deploy/deploy.ps1
@@ -0,0 +1,11 @@
+param(
+    [string]$CI_COMMIT_REF_NAME
+)
+
+# Load the Docker image from the tar file
+Write-Host "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+docker load -i "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+
+# Run the Docker container
+Write-Host "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\config\application.properties"
+docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
---
+++ test/testcases/getoneUser.ps1
@@ -0,0 +1,39 @@
+$expectedOutput = @{
+    "trial" = "bob1";
+}
+
+# Set the login credentials
+$username = "bob"
+$password = "temp1234!"
+
+# Create headers with Content-Type
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+
+# Prepare the login request body
+$loginBody = @{
+    username = $username
+    password = $password
+} | ConvertTo-Json
+
+# Perform the login request and store the session in a session variable
+$response = Invoke-RestMethod 'http://localhost:8080/login' -Method 'POST' -Headers $headers -Body $loginBody -SessionVariable session
+
+# Perform authenticated GET request using the session
+$response = Invoke-RestMethod 'http://localhost:8080/users/bob' -Method 'GET' -Headers $headers -WebSession $session
+
+if($response.username -eq $expectedOutput["trial"]) {
+        Write-Host "trial PASSED" -ForegroundColor Green
+    }
+else {
+ $example =  $expectedOutput["trial"]
+$result = $response.username
+        Write-Host "trial FAILED. Expected: $example but got $result" -ForegroundColor Red
+    }
+
+Write-Host "Hello"
+
+Write-Host $response
+
+# Output the response
+$response | ConvertTo-Json
