2aecd4ef86fbc3947fae02cea166d925f41f5afd
Roy <weichai.royston.yeo@stengg.com>
2023-10-11 17:19:49 +0800

    test

--- test/test1.ps1
+++ test/test1.ps1
@@ -2,7 +2,7 @@ $expectedOutput = @{
     "loginwith.ps1" = "bob";
 }
 
-$testCasesPath = "$BIN_REPO_SVR_PATH\l1ds\$CI_COMMIT_REF_NAME\test\test_cases\"
+$testCasesPath = "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\test\test_cases\"
 
 Get-ChildItem -Path $testCasesPath -Filter *.ps1 | ForEach-Object {
     $scriptName = $_.Name
