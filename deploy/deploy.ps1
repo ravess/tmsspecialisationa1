4f813d6025d9b4a299478c2d5703c4eb0cad7a45
Roy <weichai.royston.yeo@stengg.com>
2023-10-11 14:29:31 +0800

    why

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -2,7 +2,7 @@ param(
     [string]$CI_COMMIT_REF_NAME
 )
 
-# Load the Docker image from the tar file
+# Load the Docker image from the tar file !!!
 Write-Host: "It is running deploy.ps1......"
 Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
 # docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
