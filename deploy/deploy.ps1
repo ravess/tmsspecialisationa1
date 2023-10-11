d0384d5b09ffa2c7ec59638b37a9161381df9857
Roy <weichai.royston.yeo@stengg.com>
2023-10-11 14:28:19 +0800

    t

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -5,8 +5,8 @@ param(
 # Load the Docker image from the tar file
 Write-Host: "It is running deploy.ps1......"
 Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
-docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+# docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
 
 # Run the Docker container !
 Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties"
-docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
+# docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
