ee8386ea95005f2bad8a67220619ec1f3466bff6
Roy <weichai.royston.yeo@stengg.com>
2023-10-11 14:59:50 +0800

    test2

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -2,19 +2,13 @@ param(
     [string]$CI_COMMIT_REF_NAME
 )
 
-try {
-    # Load the Docker image from the tar file !!!
+
+# Load the Docker image from the tar file !!!
 Write-Host: "It is running deploy.ps1......"
 Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
-# docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
 
 # Run the Docker container !
 Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties"
-# docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
-} catch {
-    $errorString = "Error: $_"
-    Write-Output $errorString
-    # You can also log the error to a file
-    $errorString | Out-File -Append -FilePath "error.log"
-}
+docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
 
