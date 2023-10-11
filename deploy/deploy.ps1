c4bdd1f237d6cb401a709f1694afec131e139abe
Roy <weichai.royston.yeo@stengg.com>
2023-10-11 10:38:06 +0800

    test deploy script

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -3,9 +3,9 @@ param(
 )
 
 # Load the Docker image from the tar file
-Write-Host "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
-docker load -i "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+Write-Host "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
+docker load -i "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\bin\$CI_COMMIT_REF_NAME.tar"
 
 # Run the Docker container
 Write-Host "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\config\application.properties"
-docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\group1\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
+docker run --name $CI_COMMIT_REF_NAME --env-file "C:\Users\l1ds\bytebrigade\$CI_COMMIT_REF_NAME\config\application.properties" -p 8080:8080 -d $CI_COMMIT_REF_NAME
